70a30ce7aa2d4df8df5c175a83ff1ed8ceb9561f
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-08-07 17:08:12 +0200

    Set TextViews to data returned from WeatherResponse

--- app/src/main/java/com/marwaeltayeb/weatherforecast/view/MainActivity.kt
+++ app/src/main/java/com/marwaeltayeb/weatherforecast/view/MainActivity.kt
@@ -1,36 +1,60 @@
 package com.marwaeltayeb.weatherforecast.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.marwaeltayeb.weatherforecast.R
 import com.marwaeltayeb.weatherforecast.model.CurrentWeatherResponse
 import com.marwaeltayeb.weatherforecast.model.MainContract
 import com.marwaeltayeb.weatherforecast.presenter.MainPresenter
+import com.marwaeltayeb.weatherforecast.utils.Time
+
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity(), MainContract.View {
 
-    private lateinit var textView: TextView
+    private lateinit var txtTemperature: TextView
+    private lateinit var txtHighTemperature: TextView
+    private lateinit var txtLowTemperature: TextView
+    private lateinit var txtWeatherDescription:TextView
+    private lateinit var txtLocation:TextView
+    private lateinit var txtLastUpdated:TextView
+
     private lateinit var presenter: MainPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView = findViewById(R.id.weatherData)
+        txtTemperature = findViewById(R.id.txtTemperature)
+        txtHighTemperature = findViewById(R.id.txtHighTemperature)
+        txtLowTemperature = findViewById(R.id.txtLowTemperature)
+        txtWeatherDescription = findViewById(R.id.txtWeatherDescription)
+        txtLocation = findViewById(R.id.txtLocation)
+        txtLastUpdated = findViewById(R.id.txtLastUpdated)
 
         presenter = MainPresenter(this)
         presenter.startLoadingData()
-
     }
 
     override fun onLoadFinished(currentWeatherResponse: CurrentWeatherResponse?) {
         if (currentWeatherResponse != null) {
-            //textView.text = currentWeatherResponse.main.tempMax.toString()
-            textView.text = currentWeatherResponse.weather[0].description
+            val temperature = getString(R.string.temperature, currentWeatherResponse.main.temp.toInt())
+            txtTemperature.text = temperature
+            val highTemperature = getString(R.string.high_temperature, currentWeatherResponse.main.tempMax.toInt())
+            txtHighTemperature.text = highTemperature
+            val lowTemperature = getString(R.string.low_temperature, currentWeatherResponse.main.tempMin.toInt())
+            txtLowTemperature.text = lowTemperature
+            txtWeatherDescription.text = currentWeatherResponse.weather[0].description
+            txtLocation.text = currentWeatherResponse.name
+            val lastUpdated = getString(R.string.lastUpdated, Time.timeConverter(currentWeatherResponse.dt))
+            txtLastUpdated.text = lastUpdated
         }
     }
 
