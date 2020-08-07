8d06cfa08a72d5e013d703fd5805a10b7302c127
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-08-07 17:09:34 +0200

    Create DetailsActivity

---
+++ app/src/main/java/com/marwaeltayeb/weatherforecast/view/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.marwaeltayeb.weatherforecast.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.marwaeltayeb.weatherforecast.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
