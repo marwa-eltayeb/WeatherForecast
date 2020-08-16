7276eb7d2e8b865ebb95d937b3c7d693b07344de
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-08-16 23:15:03 +0200

    Modify WeatherService

--- app/src/main/java/com/marwaeltayeb/weatherforecast/data/WeatherService.kt
+++ app/src/main/java/com/marwaeltayeb/weatherforecast/data/WeatherService.kt
@@ -1,6 +1,7 @@
 package com.marwaeltayeb.weatherforecast.data
 
-import com.marwaeltayeb.weatherforecast.model.CurrentWeatherResponse
+import com.marwaeltayeb.weatherforecast.model.current.CurrentWeatherResponse
+import com.marwaeltayeb.weatherforecast.model.details.FullDetailsResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -14,4 +15,12 @@ interface WeatherService {
         @Query("appid") apiKey: String
     ): Call<CurrentWeatherResponse>
 
+    @GET("onecall")
+    fun getDetailedWeatherData(
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+        @Query("units") unitName: String,
+        @Query("exclude") exclude: String,
+        @Query("appid") apiKey: String
+    ): Call<FullDetailsResponse>
 }
