be19f345ca4f2591dd6fa5a7bec5a857f20f5a64
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-08-17 21:54:01 +0200

    Convert time to date

--- app/src/main/java/com/marwaeltayeb/weatherforecast/utils/Time.kt
+++ app/src/main/java/com/marwaeltayeb/weatherforecast/utils/Time.kt
@@ -10,5 +10,12 @@ class Time {
 
             return sdf.format(date);
         }
+
+        fun timeConverterToDate(timestamp: Long) : String{
+            val sdf = java.text.SimpleDateFormat("EEE',' MMM d")
+            val date = java.util.Date(timestamp * 1000)
+
+            return sdf.format(date);
+        }
     }
 }
