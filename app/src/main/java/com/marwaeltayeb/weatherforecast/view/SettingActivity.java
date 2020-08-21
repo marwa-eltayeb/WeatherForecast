64bc83307f2ab4b9fca349b44a4f7c54418308c5
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-08-21 17:53:50 +0200

    Create SettingActivity

---
+++ app/src/main/java/com/marwaeltayeb/weatherforecast/view/SettingActivity.java
@@ -0,0 +1,16 @@
+package com.marwaeltayeb.weatherforecast.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.marwaeltayeb.weatherforecast.R;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
